40df52da8f995149ca13d9e59ce8124e805bf101
Adam Kalamondo <kalmndo@gmail.com>
2023-03-07 16:38:06 +0700

    font

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const interFont = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  preload: true,
+  display: "swap",
+});
 
 export default function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-inter: ${interFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- apps/web/pages/create-vote/index.tsx
+++ apps/web/pages/create-vote/index.tsx
@@ -3,7 +3,9 @@ import Layout from "../../components/layout/Layout";
 export default function createVote() {
   return (
     <Layout>
-      create vote
+      <div className="flex flex-col w-full min-h-screen items-center">
+        create vote
+      </div>
     </Layout>
   );
 }
