b6e8b333ccfed99e06be179a5c39c462b855b6a5
Adam Kalamondo <kalmndo@gmail.com>
2023-03-11 19:18:58 +0700

    index

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,9 @@
+import Layout from "../components/layout/Layout";
 
 export default function Web() {
   return (
-    <div>
-      <h1 className="bg-gray-100 font-sans w-full min-h-screen m-0">Web</h1>
-    </div>
+    <Layout>
+      <div>halo</div>
+    </Layout>
   );
 }
