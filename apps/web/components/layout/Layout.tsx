1bd67227f676e6346df66a558612cdee7dd7c1e8
Adam Kalamondo <kalmndo@gmail.com>
2023-03-12 20:37:00 +0700

    remove header change to navbar

--- apps/web/components/layout/Layout.tsx
+++ apps/web/components/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
 import { PropsWithChildren } from "react";
+import Navbar from "../navbar";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div>
-      <Header />
-        <div className="bg-white flex flex-col w-full min-h-screen items-center">
-          <div className="max-w-3xl	w-full">{children}</div>
-        </div>
+      <Navbar />
+      <div className="bg-white flex flex-col w-full min-h-screen items-center">
+        <div className="max-w-3xl	w-full">{children}</div>
+      </div>
     </div>
   );
 }
