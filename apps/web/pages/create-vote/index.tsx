ccb8a27818cf22899bc4e1a63547b88eba761ec8
Adam Kalamondo <kalmndo@gmail.com>
2023-03-13 20:52:26 +0700

    datetimepicker

--- apps/web/pages/create-vote/index.tsx
+++ apps/web/pages/create-vote/index.tsx
@@ -8,7 +8,7 @@ export default function createVote() {
   return (
     <Layout>
       <Header title="Create Vote" subtitle="Create vote to share for people" />
-      <div className="space-y-8">
+      <div className="space-y-8 mb-40">
         <InputField name="Title" />
         <InputField name="Description" />
         <Candidates />
---
+++ packages/ui/dateTimePicker/DateTimePicker.tsx
@@ -0,0 +1,6 @@
+export const DateTimePicker = () => {
+    return (
+    <div>
+    </div>
+    )
+  }
